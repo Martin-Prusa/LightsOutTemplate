f1c4b459ef353b401c562e835e216e29ad80055a
Martin-Prusa <martin.prusa@educanet.cz>
2021-11-19 13:30:33 +0100

    dialog

--- src/main/java/cz/educanet/lights/out/data/Data.java
+++ src/main/java/cz/educanet/lights/out/data/Data.java
@@ -1,14 +1,18 @@
 package cz.educanet.lights.out.data;
 
 import com.google.gson.Gson;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.*;
 
 public class Data {
     public void saveData(boolean[][] field) {
         try {
+            FileChooser chooser = new FileChooser();
+            File file = chooser.showSaveDialog(new Stage());
             Gson gson = new Gson();
-            BufferedWriter bw = new BufferedWriter(new FileWriter("save.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
             bw.write(gson.toJson(field));
             bw.close();
         } catch (IOException e) {}
@@ -17,8 +21,10 @@ public class Data {
 
     public boolean[][] loadData() {
         try {
+            FileChooser chooser = new FileChooser();
+            File file = chooser.showOpenDialog(new Stage());
             Gson gson = new Gson();
-            BufferedReader br = new BufferedReader(new FileReader("save.txt"));
+            BufferedReader br = new BufferedReader(new FileReader(file));
             String f = br.readLine();
             br.close();
             return gson.fromJson(f, boolean[][].class);
